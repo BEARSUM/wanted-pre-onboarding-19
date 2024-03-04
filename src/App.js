041865ff7c899c6fb49545533f9d0b1c7b2a972f
[SW5] 문수민 <sum95771@gmail.com>
2024-03-04 16:54:26 +0900

    feat: redux 설치 및 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Provider as TodosProvider } from "react-redux";
+import store from "./store/store";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <TodosProvider store={store}>
+        <main></main>
+      </TodosProvider>
+    </div>
+  );
 }
 
 export default App;
